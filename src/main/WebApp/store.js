e146be95bea5797af45f2f2fbc87a5c73e6b23b7
Alperen Elcik <alperen.elcik@hva.nl>
2022-07-01 10:53:05 +0200

    js aangepast om de buttons altijd werkend te hebben door een ready function toe te voegen source: Web Dev Simplified

--- src/main/WebApp/store.js
+++ src/main/WebApp/store.js
@@ -1,13 +1,37 @@
+// checken of de body al gemaakt is anders kan js niet de html buttons vinden source: Web Dev Simplified
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    ready()
+}
+// met deze ready functie zorg ik er dus voor dat de buttons altijd zullen werken ondanks dat de pagina nog niet is geladen
+function ready() {
+    var removeItemFromCart = document.getElementsByClassName('btn-danger')
+// test voor de remove buttons van de bovenste class
+    console.log(removeItemFromCart)
+// loop door alle buttons in de cart en zet de removeknop de index ervan vast aan de button.
+// daarna wordt er gecontroleerd wanneer er op de remove knop wordt gedrukt met een click eventlistener
+    for (var i = 0; i < removeItemFromCart.length; i++) {
+        var button = removeItemFromCart[i]
+        // een event creeeren wanneer er op de knop wordt gedrukt dan wordt de hele row van de cart verwijderd
+        // door de parent div van de cart te lezen en verwijderen.
+        button.addEventListener('click', function (event) {
+            console.log('clicked')
+            var buttonClicked = event.target
+            buttonClicked.parentElement.parentElement.remove()
 
-
+            updatePriceTotal()
+        })
+    }
+}
 
 
 // methode om de totale prijs te updaten
-function updatePriceTotal(){
+function updatePriceTotal() {
     var cartItemContainer = document.getElementsByClassName('cart-items')[0]  //pakt de eerste item
     var cartRows = cartItemContainer.getElementsByClassName('cart-row')  //pakt alle rijen
     var total = 0 //hebben we later nodig (dit gaat door de loop heen)
-    for(var i =0; i<cartRows.length; i++) {
+    for (var i = 0; i < cartRows.length; i++) {
         var cartROw = cartRows[i]
         var priceELement = cartROw.getElementsByClassName('cart-price')[0]
         var quantityElement = cartROw.getElementsByClassName('cart-quantity-input')[0]
